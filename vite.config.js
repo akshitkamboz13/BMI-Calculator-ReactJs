e275fdd0c4f42029a9341083aaa00b6d1f779bd2
Akshit Kamboj <akshitkamboz13@gmail.com>
2024-05-09 21:34:11 +0530

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BMI-Calculator/'
+  base: '/BMI-Calculator/',
+  base: '/https://akshitkamboz13.github.io/BMI-Calculator-ReactJs/'
 })
